f8b5a28d13c43fb3e8dadbf4ddf05242dfa37f64
Corentin Boblet <corentinboblet@gmail.com>
2025-06-11 23:07:35 +0200

    Started creating a MetaIA skeletton

---
+++ MetaIA.cpp
@@ -0,0 +1,65 @@
+﻿#include "MetaIA.hpp"
+
+// Survive
+std::vector<const lux::City&> MetaIA::GetNeedingCity(int turn) const
+{
+    // return all city tiles that need to be given resources
+    std::vector<const lux::City&> needyCity;
+    
+    for (const lux::City& city : m_cities)
+    {
+        // TODO add only if city doesnt meet fuel requirements
+        needyCity.emplace_back(city);
+    }
+    return needyCity;
+}
+
+std::vector<const lux::Unit&> MetaIA::GetNeedingUnits(int turn) const
+{
+    // return all Units that need resources
+    std::vector<const lux::Unit&> needyUnits;
+    
+    for (const lux::Unit& unit : m_units)
+    {
+        // TODO add only if city doesnt meet fuel requirements
+        needyUnits.emplace_back(unit);
+    }
+    return needyUnits;
+}
+
+// Expand
+bool MetaIA::CanBuildUnit() const
+{
+    size_t cityTilesNb = 0;
+
+    for (const lux::City& city : m_cities)
+    {
+        cityTilesNb += city.citytiles.size();
+    }
+
+    return cityTilesNb < m_units.size();
+}
+
+// Other
+void MetaIA::Update(int turn)
+{
+    
+    // Survive
+    for (auto city : GetNeedingCity(turn))
+    {
+        // TODO make city collect resources
+    }
+
+    for (auto unit : GetNeedingUnits(turn))
+    {
+        // TODO make unit collect resources
+    }
+
+    // Expand
+    if(CanBuildUnit())
+    {
+        // TODO find a suitable city tile to build unit on
+    }
+
+    
+}
---
+++ MetaIA.hpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <vector>
+
+#include "lux/city.hpp"
+#include "lux/game_objects.hpp"
+
+class MetaIA
+{
+    /*  
+     *  Goals one after the other
+     *  - Survive (keep units and city alive through each night)
+     *  - Expand (more cities, more units, roads, research)
+     *  - Attack (go annoy the ennemy, not necessary)
+     */
+private:
+    // Survive
+    std::vector<lux::City> m_cities;
+    std::vector<lux::Unit> m_units;
+
+    std::vector<const lux::City&> GetNeedingCity(int turn) const;
+    std::vector<const lux::Unit&> GetNeedingUnits(int turn) const;
+
+    //Expand
+    
+    
+    bool CanBuildUnit() const;
+    
+public:
+    void Update(int turn);
+};
