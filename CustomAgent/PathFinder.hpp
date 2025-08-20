16ba796948ae324072358f039c97208bd7709804
p.luneau <paul.luneau@mail.com>
2025-08-20 09:47:01 +0200

    Improved the Pathfinder by using a better cost function

--- CustomAgent/PathFinder.hpp
+++ CustomAgent/PathFinder.hpp
@@ -48,11 +48,10 @@ public:
 class PathFinder
 {
 public:
-    static bool FindPath(const GameMap& map, Position& startPosition, Position& targetPosition, std::vector<DIRECTIONS>& o_pathToTarget)
+    static bool FindPath(const GameMap& map, const Position& startPosition, const Position& targetPosition, const Player& curentPlayer, std::vector<DIRECTIONS>& o_pathToTarget)
     {
-        int cellsCount = map.height * map.width;
-        std::vector<PathfinderCell> allCells = {};
-        allCells.reserve(cellsCount);
+        const int cellsCount = map.height * map.width;
+        std::vector<PathfinderCell> allCells { cellsCount };
 
         for (int x = 0; x < map.width; ++x)
         {    
@@ -96,7 +95,7 @@ public:
 
                 PathfinderCell& neighbouringCell = allCells[PositionToArrayIndex(neighbouringPosition, map)];
                 
-                float gScoreAttempt = currentCell.GScore + ComputeCost(currentCell, neighbouringCell);
+                float gScoreAttempt = currentCell.GScore + ComputeCost(currentCell, neighbouringCell, curentPlayer);
                 if (gScoreAttempt >= neighbouringCell.GScore)
                 {
                     continue;
@@ -119,17 +118,43 @@ public:
     }
 
 private:
-    static float ComputeHeuristic(lux::Position& cellPosition, lux::Position& targetPosition)
+    static float ComputeHeuristic(const lux::Position& cellPosition, const lux::Position& targetPosition)
     {
         return cellPosition.distanceTo(targetPosition);
     }
 
-    static float ComputeCost(const PathfinderCell& currentCell, const PathfinderCell& neighbouringCell)
+    static float ComputeCost(const PathfinderCell& currentCell, const PathfinderCell& neighbouringCell, const Player& currentPlayer)
     {
+        CityTile* neighbouringCityTile = neighbouringCell.Cell->citytile;
+        if (neighbouringCityTile != nullptr && neighbouringCityTile->team != currentPlayer.team)
+        {
+            return 99999999.0f; // Can't pass through a opponent's city tile
+        }
+
+        bool allyUnitInNeighbouringCell = false;
+        for (const Unit& allyUnit : currentPlayer.units) 
+        {
+            if (allyUnit.pos == neighbouringCell.Cell->pos) 
+            {
+                allyUnitInNeighbouringCell = true;
+                break;
+            }
+        }
+
+        if (allyUnitInNeighbouringCell && neighbouringCityTile == nullptr)
+        {
+            return 99999999.0f; // Can't pass through an ally outside a city
+        }
+
+        if (neighbouringCell.Cell->road > 0.0f) 
+        {
+            return 1.0f / neighbouringCell.Cell->road;
+        }
+
         return 1.0f;
     }
 
-    static int PositionToArrayIndex(lux::Position& position, const lux::GameMap& map)
+    static int PositionToArrayIndex(const lux::Position& position, const lux::GameMap& map)
     {
         return PositionToArrayIndex(position.x, position.y, map);
     }
