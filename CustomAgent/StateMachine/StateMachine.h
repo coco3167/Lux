26a733685ca4efc3429b27e3f121abccde2701d9
p.luneau <paul.luneau@mail.com>
2025-08-20 09:42:13 +0200

    State machine base classes

---
+++ CustomAgent/StateMachine/StateMachine.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <memory>
+#include "SMState.h"
+
+
+template<typename TSMInfos>
+class StateMachine
+{
+private:
+	std::unique_ptr<SMState<TSMInfos>> m_currentState;
+
+public:
+	StateMachine(std::unique_ptr<SMState<TSMInfos>> startState) :
+		m_currentState(std::move(startState))
+	{
+		m_currentState->StartState();
+	}
+
+	void Update(TSMInfos& stateInfos) 
+	{
+		std::unique_ptr<SMState<TSMInfos>> requestedState = m_currentState->UpdateState(stateInfos);
+		if (requestedState) 
+		{
+			ChangeState(std::move(requestedState));
+		}
+	}
+
+	void ChangeState(std::unique_ptr<SMState<TSMInfos>> newState) 
+	{
+		m_currentState->EndState();
+		m_currentState = std::move(newState);
+		m_currentState->StartState();
+	}
+};
+
