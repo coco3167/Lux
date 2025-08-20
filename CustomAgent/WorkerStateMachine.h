3190c8661cb1922c9d12a8e8938036863592cd00
p.luneau <paul.luneau@mail.com>
2025-08-20 19:35:13 +0200

    Moved the worker's states definitions in a cpp file

--- CustomAgent/WorkerStateMachine.h
+++ CustomAgent/WorkerStateMachine.h
@@ -44,135 +44,35 @@ namespace WorkerSM
 		}
 	};
 
-	class DefaultState;
-	class MovingState;
-	class BuildingCityState;
-	class CollectingRessourcesState;
-
 	class DefaultState : public SMState<WorkerSMInfos>
 	{
 	public:
 
-		DefaultState(WorkerSMInfos& stateInfos) 
-		{
-			stateInfos.CurrentObjective = Objective::None;
-		}
+		DefaultState(WorkerSMInfos& stateInfos);
 
-		virtual std::unique_ptr<SMState<WorkerSMInfos>> UpdateState(WorkerSMInfos& stateInfos) override
-		{
-			switch (stateInfos.CurrentObjective) 
-			{
-				case Objective::None:
-					return nullptr;
-
-				case Objective::CollectRessourceForSelf:
-				case Objective::CollectRessourceForCity:
-					Cell* targetCell = stateInfos.Datas.GetClosestResourceCell(stateInfos.ControlledWorker.pos);
-					return std::unique_ptr<MovingState>(new MovingState(stateInfos, targetCell->pos));
-
-				case Objective::BuildCity:
-					Cell* cityBuildTile = stateInfos.Datas.GetBestCityBuildingCell(stateInfos.ControlledWorker.pos);
-					return std::unique_ptr<MovingState>(new MovingState(stateInfos, cityBuildTile->pos));
-
-			}
-		}
+		virtual std::unique_ptr<SMState<WorkerSMInfos>> UpdateState(WorkerSMInfos& stateInfos) override;
 	};
 
 	class MovingState : public SMState<WorkerSMInfos>
 	{
 	public:
-		MovingState(WorkerSMInfos& stateInfos, const Position& target)
-		{
-			stateInfos.TargetPosition = target;
-		}
+		MovingState(WorkerSMInfos& stateInfos, const Position& target);
 
-		virtual std::unique_ptr<SMState<WorkerSMInfos>> UpdateState(WorkerSMInfos& stateInfos) override
-		{
-			Unit& unit = stateInfos.ControlledWorker;
+		virtual std::unique_ptr<SMState<WorkerSMInfos>> UpdateState(WorkerSMInfos& stateInfos) override;
 
-			if (unit.pos == stateInfos.TargetPosition) 
-			{
-				return std::move(NextState(stateInfos));
-			}
-
-
-			if (!unit.canAct())
-			{
-				return nullptr;
-			}
-
-			std::vector<DIRECTIONS> pathToTarget{};
-			pathToTarget.reserve(10);
-
-			bool pathFound = PathFinder::FindPath(stateInfos.Datas.Map, unit.pos, stateInfos.TargetPosition, stateInfos.Datas.Owner, pathToTarget);
-
-			if (!pathFound) 
-			{
-				return nullptr;
-			}
-
-			stateInfos.Datas.AddAction(std::move(unit.move(pathToTarget[0])));
-
-			return nullptr;
-		}
-
-		std::unique_ptr<SMState<WorkerSMInfos>> NextState(WorkerSMInfos& stateInfos) 
-		{
-			switch (stateInfos.CurrentObjective) 
-			{
-				case Objective::BuildCity:
-					return std::unique_ptr<BuildingCityState>(new BuildingCityState());
-
-				case Objective::CollectRessourceForSelf:
-				case Objective::CollectRessourceForCity:
-					return std::unique_ptr<CollectingRessourcesState>(new CollectingRessourcesState());
-
-			}
-		}
+		std::unique_ptr<SMState<WorkerSMInfos>> NextState(WorkerSMInfos& stateInfos);
 	};
 
 	class BuildingCityState : public SMState<WorkerSMInfos>
 	{
-		virtual std::unique_ptr<SMState<WorkerSMInfos>> UpdateState(WorkerSMInfos& stateInfos) override
-		{
-			Unit& unit = stateInfos.ControlledWorker;
-			if (!unit.canAct())
-			{
-				return nullptr;
-			}
-
-			stateInfos.Datas.AddAction(std::move(unit.buildCity()));
-
-			return std::unique_ptr<DefaultState>(new DefaultState(stateInfos));
-		}
+		virtual std::unique_ptr<SMState<WorkerSMInfos>> UpdateState(WorkerSMInfos& stateInfos) override;
 	};
 
 	class CollectingRessourcesState : public SMState<WorkerSMInfos>
 	{
-		virtual std::unique_ptr<SMState<WorkerSMInfos>> UpdateState(WorkerSMInfos& stateInfos) override
-		{
-			Unit& unit = stateInfos.ControlledWorker;
+		virtual std::unique_ptr<SMState<WorkerSMInfos>> UpdateState(WorkerSMInfos& stateInfos) override;
 
-			if (unit.getCargoSpaceLeft() == 0) 
-			{
-				return std::move(NextState(stateInfos));
-			}
-
-			return nullptr;
-		}
-
-		std::unique_ptr<SMState<WorkerSMInfos>> NextState(WorkerSMInfos& stateInfos)
-		{
-			switch (stateInfos.CurrentObjective)
-			{
-			case Objective::CollectRessourceForSelf:
-				return std::unique_ptr<DefaultState>(new DefaultState(stateInfos));
-
-			case Objective::CollectRessourceForCity:
-				const CityTile* closestTile = PathFinder::GetClosestCityTile(stateInfos.ControlledWorker.pos, stateInfos.SuppliedCity, stateInfos.Datas.Map, stateInfos.Datas.Owner);
-				return std::unique_ptr<MovingState>(new MovingState(stateInfos, closestTile->pos));
-			}
-		}
+		std::unique_ptr<SMState<WorkerSMInfos>> NextState(WorkerSMInfos& stateInfos);
 	};
 }
 
