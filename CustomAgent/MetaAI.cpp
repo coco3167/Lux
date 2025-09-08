5535608b382adb1a687b7feb1b80a10aa806440f
p.luneau <paul.luneau@mail.com>
2025-09-08 14:32:11 +0200

    That was the problem all along ?

--- CustomAgent/MetaAI.cpp
+++ CustomAgent/MetaAI.cpp
@@ -10,6 +10,10 @@ MetaAI::MetaAI(GameDatas* gameDatas):
 {
     m_gameDatas->WorkerAIs = &m_workerAIs;
     m_gameDatas->CartAIs = &m_cartAIs;
+
+    m_workerAIs.reserve(512);
+    m_cartAIs.reserve(512);
+    m_cityAIs.reserve(512);
 }
 
 // Other
